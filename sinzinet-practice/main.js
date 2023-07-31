b285798efd9c6d20130fdce51e31e7747eaeac08
Heesoo Kim <heesoo0323@gmail.com>
2023-07-31 15:34:56 +0900

    Add files via upload

---
+++ sinzinet-practice/main.js
@@ -0,0 +1,394 @@
+var obj = 
+    {
+        "machineDataA": {
+            "machineId": "A",
+            "machineTemp": 80.0,
+            "sensor" : {
+                "sensor1": {
+                    "location": {
+                        "ripple": {
+                            "x": 697,
+                            "y": 207
+                        },
+                        "number": {
+                            "x": 743,
+                            "y": 304
+                        }
+                    },
+                    "sensorNum": 1,
+                    "sensorLevel": 1
+                },
+                "sensor2": {
+                    "location": {
+                        "ripple": {
+                            "x": 847,
+                            "y": 315
+                        },
+                        "number": {
+                            "x": 892,
+                            "y": 414
+                        }
+                    },
+                    "sensorNum": 2,
+                    "sensorLevel": 1
+                },
+                "sensor3": {
+                    "location": {
+                        "ripple": {
+                            "x": 968,
+                            "y": 160
+                        },
+                        "number": {
+                            "x": 1016,
+                            "y": 259
+                        }
+                    },
+                    "sensorNum": 3,
+                    "sensorLevel": 1
+                },
+                "sensor4": {
+                    "location": {
+                        "ripple": {
+                            "x": 1038,
+                            "y": 371
+                        },
+                        "number": {
+                            "x": 1093,
+                            "y": 469
+                        }
+                    },
+                    "sensorNum": 4,
+                    "sensorLevel": 1
+                }
+            }
+        },
+        "machineDataB": {
+            "machineId": "B",
+            "machineTemp": 80.0,
+            "sensor" : {
+                "sensor1": {
+                    "location": {
+                        "ripple": {
+                            "x": 601,
+                            "y": 254
+                        },
+                        "number": {
+                            "x": 645,
+                            "y": 350
+                        }
+                    },
+                    "sensorNum": 1,
+                    "sensorLevel": 1
+                },
+                "sensor2": {
+                    "location": {
+                        "ripple": {
+                            "x": 723,
+                            "y": 374
+                        },
+                        "number": {
+                            "x": 767,
+                            "y": 472
+                        }
+                    },
+                    "sensorNum": 2,
+                    "sensorLevel": 1
+                },
+                "sensor3": {
+                    "location": {
+                        "ripple": {
+                            "x": 880,
+                            "y": 115
+                        },
+                        "number": {
+                            "x": 926,
+                            "y": 211
+                        }
+                    },
+                    "sensorNum": 3,
+                    "sensorLevel": 1
+                },
+                "sensor4": {
+                    "location": {
+                        "ripple": {
+                            "x": 887,
+                            "y": 444
+                        },
+                        "number": {
+                            "x": 941,
+                            "y": 543
+                        }
+                    },
+                    "sensorNum": 4,
+                    "sensorLevel": 1
+                }
+            }
+        },
+            "machineDataC": {
+                "machineId": "C",
+                "machineTemp": 80.0,
+                "sensor" : {
+                    "sensor1": {
+                        "location": {
+                            "ripple": {
+                                "x": 586,
+                                "y": 103
+                            },
+                            "number": {
+                                "x": 632,
+                                "y": 197
+                            }
+                        },
+                        "sensorNum": 1,
+                        "sensorLevel": 1
+                    },
+                    "sensor2": {
+                        "location": {
+                            "ripple": {
+                                "x": 634,
+                                "y": 267
+                            },
+                            "number": {
+                                "x": 679,
+                                "y": 364
+                            }
+                        },
+                        "sensorNum": 2,
+                        "sensorLevel": 1
+                    },
+                    "sensor3": {
+                        "location": {
+                            "ripple": {
+                                "x": 730,
+                                "y": 389
+                            },
+                            "number": {
+                                "x": 777,
+                                "y": 487
+                            }
+                        },
+                        "sensorNum": 3,
+                        "sensorLevel": 1
+                    },
+                    "sensor4": {
+                        "location": {
+                            "ripple": {
+                                "x": 1124,
+                                "y": 288
+                            },
+                            "number": {
+                                "x": 1176,
+                                "y": 381
+                            }
+                        },
+                        "sensorNum": 4,
+                        "sensorLevel": 1
+                    }
+                }
+            },
+            "machineDataD": {
+                "machineId": "D",
+                "machineTemp": 80.0,
+                "sensor" : {
+                    "sensor1": {
+                        "location": {
+                            "ripple": {
+                                "x": 662,
+                                "y": 300
+                            },
+                            "number": {
+                                "x": 706,
+                                "y": 395
+                            }
+                        },
+                        "sensorNum": 1,
+                        "sensorLevel": 1
+                    },
+                    "sensor2": {
+                        "location": {
+                            "ripple": {
+                                "x": 769,
+                                "y": 192
+                            },
+                            "number": {
+                                "x": 815,
+                                "y": 286
+                            }
+                        },
+                        "sensorNum": 2,
+                        "sensorLevel": 1
+                    },
+                    "sensor3": {
+                        "location": {
+                            "ripple": {
+                                "x": 923,
+                                "y": 182
+                            },
+                            "number": {
+                                "x": 979,
+                                "y": 275
+                            }
+                        },
+                        "sensorNum": 3,
+                        "sensorLevel": 1
+                    },
+                    "sensor4": {
+                        "location": {
+                            "ripple": {
+                                "x": 1124,
+                                "y": 288
+                            },
+                            "number": {
+                                "x": 1176,
+                                "y": 382
+                            }
+                        },
+                        "sensorNum": 4,
+                        "sensorLevel": 1
+                    }
+                }
+            },
+            "machineDataE": {
+                "machineId": "E",
+                "machineTemp": 80.0,
+                "sensor" : {
+                    "sensor1": {
+                        "location": {
+                            "ripple": {
+                                "x": 615,
+                                "y": 332
+                            },
+                            "number": {
+                                "x": 661,
+                                "y": 431
+                            }
+                        },
+                        "sensorNum": 1,
+                        "sensorLevel": 1
+                    },
+                    "sensor2": {
+                        "location": {
+                            "ripple": {
+                                "x": 781,
+                                "y": 325
+                            },
+                            "number": {
+                                "x": 827,
+                                "y": 421
+                            }
+                        },
+                        "sensorNum": 2,
+                        "sensorLevel": 1
+                    },
+                    "sensor3": {
+                        "location": {
+                            "ripple": {
+                                "x": 959,
+                                "y": 306
+                            },
+                            "number": {
+                                "x": 1007,
+                                "y": 401
+                            }
+                        },
+                        "sensorNum": 3,
+                        "sensorLevel": 1
+                    },
+                    "sensor4": {
+                        "location": {
+                            "ripple": {
+                                "x": 1124,
+                                "y": 288
+                            },
+                            "number": {
+                                "x": 1179,
+                                "y": 384
+                            }
+                        },
+                        "sensorNum": 4,
+                        "sensorLevel": 1
+                    }
+                }
+            },
+            "machineDataF": {
+                "machineId": "F",
+                "machineTemp": 80.0,
+                "sensor" : {
+                    "sensor1": {
+                        "location": {
+                            "ripple": {
+                                "x": 729,
+                                "y": 273
+                            },
+                            "number": {
+                                "x": 775,
+                                "y": 372
+                            }
+                        },
+                        "sensorNum": 1,
+                        "sensorLevel": 1
+                    },
+                    "sensor2": {
+                        "location": {
+                            "ripple": {
+                                "x": 882,
+                                "y": 321
+                            },
+                            "number": {
+                                "x": 928,
+                                "y": 417
+                            }
+                        },
+                        "sensorNum": 2,
+                        "sensorLevel": 1
+                    },
+                    "sensor3": {
+                        "location": {
+                            "ripple": {
+                                "x": 1062,
+                                "y": 299
+                            },
+                            "number": {
+                                "x": 1107,
+                                "y": 397
+                            }
+                        },
+                        "sensorNum": 3,
+                        "sensorLevel": 1
+                    },
+                    "sensor4": {
+                        "location": {
+                            "ripple": {
+                                "x": 1127,
+                                "y": 152
+                            },
+                            "number": {
+                                "x": 1180,
+                                "y": 248
+                            }
+                        },
+                        "sensorNum": 4,
+                        "sensorLevel": 1
+                    }
+                }
+            }
+    };
+        
+
+
+var object = {
+    first: {
+        first: '1',
+        second: {
+            second: true
+        }
+    },
+    second: {
+        first: '2'
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  };
+
